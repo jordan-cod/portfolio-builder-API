3a3c3638658b20421db75f301a96ecfc47cca3da
Gabriel Jordan <gabrijordan3@gmail.com>
2025-03-23 16:21:56 -0300

    add API key middleware to /api routes for enhanced security

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"portfolio-backend/internal/handlers"
+	"portfolio-backend/internal/midlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-	api := r.Group("/api")
+	api := r.Group("/api", midlewares.ApiKeyMiddleware)
 	{
 		api.GET("/projects", handlers.ProjectsHandler)
 		api.GET("/projects/:id", handlers.GetProjectHandler)
